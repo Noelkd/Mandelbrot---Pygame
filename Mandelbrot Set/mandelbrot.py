6f2b42d46d59ca8a2e1a51cfc81548ea8ded0cfd
Noelkd <nkdoherty@gmail.com>
2013-03-27 18:33:50 +0000

    Formatting
    Formatting removed leftover variables

--- Mandelbrot Set/mandelbrot.py
+++ Mandelbrot Set/mandelbrot.py
@@ -12,17 +12,15 @@ def exit_game():
 	sys.exit()
 
 def run_game():
-	BG_COLOR = 0, 0, 0
-	PX_COLOR = 225, 200, 200
-	
-	clock = pygame.time.Clock()
+
 	WINDOW_HEIGHT = 480
 	WINDOW_WIDTH = 640
-	MAX_ITERTIONS = 30
 	pygame.init()
 	screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 	pygame.display.set_caption("MENDEL VIEWER")
 	pygame.display.flip()
+
+	MAX_ITERTIONS = 30
 	MinRe = -2.0
 	MaxRe = 1.0
 	MinIm = -1.2
@@ -33,26 +31,29 @@ def run_game():
 
 	get_c_re = lambda x: MinRe + x * Re_factor
 	get_c_im = lambda y: MaxIm - y * Im_factor
+
 	image = Image.new("RGB",(WINDOW_WIDTH, WINDOW_HEIGHT), "black")
+
 	for y in range(1, WINDOW_HEIGHT):
 		c_im = get_c_im(y)
-		#print "c_im:" + str(c_im)
+
 		for x in range(1, WINDOW_WIDTH):
 			c_re = get_c_re(x)
-			#print "c_re:" + str(c_re) 
 			Z_re = c_re
 			Z_im = c_im
 			isInside = True
+
 			for n in range(1, MAX_ITERTIONS):
 				Z_re2 = Z_re * Z_re
 				Z_im2 = Z_im * Z_im
+
 				if (Z_re2 + Z_im2 > 4):
-					
 					isInside = False
 					break
+
 				Z_im = 2 * Z_re * Z_im + c_im
 				Z_re = Z_re2 - Z_im2 + c_re
-			
+
 			if isInside:
 				image.putpixel((x, y), (255,255,255))
 			else:
@@ -60,8 +61,11 @@ def run_game():
 
 	image.save(cwd+"\\tmp.BMP", "BMP")
 	image_surf = pygame.image.load("tmp.BMP").convert()
+
 	while True:
+
 		screen.blit(image_surf,(0,0))
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit_game()
