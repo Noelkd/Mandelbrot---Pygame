cc3454578a32ef8ac15d645f3372b6a113511931
Noelkd <nkdoherty@gmail.com>
2013-03-29 12:54:02 +0000

    Removed PIL requirement, created mandelbrot class
    Removed PIL by drawing to a pygame surface, created a class for drawing
    surfaces added key presses to change the amount of iterations used to
    calc if a pixel is in the set or not.

--- Mandelbrot Set/mandelbrot.py
+++ Mandelbrot Set/mandelbrot.py
@@ -2,7 +2,6 @@ from __future__ import division
 
 import pygame
 import sys
-import Image
 import os
 
 cwd = os.getcwd()
@@ -13,6 +12,66 @@ def exit_game():
 def save_image(image):
 	image.save(cwd+"\\tmp.BMP", "BMP")
 
+
+def get_load_screen(screen_size, window_height, window_width):
+	loading_screen = pygame.Surface(screen_size)
+	loading_screen = loading_screen.convert()
+	loading_screen.fill((0, 0, 0))
+
+	font = pygame.font.Font(None, 36)
+	text = font.render("Loading....", 1, (225, 225, 225))
+	textpos = text.get_rect()
+	textpos.centerx = window_width / 2
+	textpos.centery = window_height / 2
+	loading_screen.blit(text, textpos)
+	return loading_screen
+
+
+class MandelBrot(object):
+	def __init__(self, window_height, window_width, max_iter, MinRe, MaxRe, MinIm):
+		self.window_height = window_height
+		self.window_width = window_width
+		self.max_iter = max_iter
+		self.MinRe = MinRe
+		self.MaxRe = MaxRe
+		self.MinIm = MinIm
+		self.MaxIm = MinIm + (MaxRe-MinRe) * window_height / window_width
+		self.Re_factor = (MaxRe - MinRe) / (window_width - 1)
+		self.Im_factor = (self.MaxIm - MinIm) / (window_height - 1)
+		self.get_c_re = lambda x: self.MinRe + x * self.Re_factor
+		self.get_c_im = lambda y: self.MaxIm - y * self.Im_factor
+	
+	def get_surface(self, surface):
+		
+		for y in range(1, self.window_height):
+			c_im = self.get_c_im(y)
+
+			for x in range(1, self.window_width):
+				c_re = self.get_c_re(x)
+				Z_re = c_re
+				Z_im = c_im
+				isInside = True
+
+				for n in range(1, self.max_iter):
+					Z_re2 = Z_re * Z_re
+					Z_im2 = Z_im * Z_im
+
+					if (Z_re2 + Z_im2 > 4):
+						isInside = False
+						break
+
+					Z_im = 2 * Z_re * Z_im + c_im
+					Z_re = Z_re2 - Z_im2 + c_re
+
+				if isInside:
+					surface.fill((225,225,225),(x,y,x,y))
+
+				else:
+					surface.fill((int(225/n),1,0),(x,y,x,y))
+		return surface
+
+
+
 def run_game():
 
 	WINDOW_HEIGHT = 480
@@ -20,70 +79,31 @@ def run_game():
 	pygame.init()
 	screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 	pygame.display.set_caption("MENDEL VIEWER")
-	loading_screen = pygame.Surface(screen.get_size())
-	loading_screen = loading_screen.convert()
-	loading_screen.fill((0, 0, 0))
-
-	font = pygame.font.Font(None, 36)
-	text = font.render("Loading....", 1, (225, 225, 225))
-	textpos = text.get_rect()
-	textpos.centerx = WINDOW_WIDTH / 2
-	textpos.centery = WINDOW_HEIGHT / 2
-	
-	loading_screen.blit(text, textpos)
+	loading_screen = get_load_screen(screen.get_size(),WINDOW_HEIGHT,WINDOW_WIDTH)
 	screen.blit(loading_screen,(0,0))
 	pygame.display.flip()
-	
+
 	MAX_ITERTIONS = 30
 	MinRe = -2.0
 	MaxRe = 1.0
 	MinIm = -1.2
-	MaxIm = MinIm + (MaxRe-MinRe) * WINDOW_HEIGHT / WINDOW_WIDTH
-
-	Re_factor = (MaxRe - MinRe) / (WINDOW_WIDTH - 1)
-	Im_factor = (MaxIm - MinIm) / (WINDOW_HEIGHT - 1)
-
-	get_c_re = lambda x: MinRe + x * Re_factor
-	get_c_im = lambda y: MaxIm - y * Im_factor
-
-	image = Image.new("RGB",(WINDOW_WIDTH, WINDOW_HEIGHT), "black")
-
-	for y in range(1, WINDOW_HEIGHT):
-		c_im = get_c_im(y)
-
-		for x in range(1, WINDOW_WIDTH):
-			c_re = get_c_re(x)
-			Z_re = c_re
-			Z_im = c_im
-			isInside = True
-
-			for n in range(1, MAX_ITERTIONS):
-				Z_re2 = Z_re * Z_re
-				Z_im2 = Z_im * Z_im
-
-				if (Z_re2 + Z_im2 > 4):
-					isInside = False
-					break
-
-				Z_im = 2 * Z_re * Z_im + c_im
-				Z_re = Z_re2 - Z_im2 + c_re
-
-			if isInside:
-				image.putpixel((x, y), (int(255/n),255,255))
-			else:
-				image.putpixel((x, y), (int(225/n),0,0))
-
-	
-	image_surf = pygame.image.frombuffer(image.tostring(), image.size, image.mode)
-
+	mandel_maker = MandelBrot(WINDOW_HEIGHT, WINDOW_WIDTH, MAX_ITERTIONS, MinRe, MaxRe, MinIm)
+	surface = mandel_maker.get_surface(pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT)))
 	while True:
-
-		screen.blit(image_surf,(0,0))
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit_game()
+			elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+				mandel_maker.max_iter += 5
+				surface = mandel_maker.get_surface(surface)
+			elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+				mandel_maker.max_iter -= 5
+				surface = mandel_maker.get_surface(surface)
+
+		screen.blit(surface,(0,0))
 		pygame.display.flip()
+		pygame.time.wait(50)
 
 
 if __name__ == "__main__":
